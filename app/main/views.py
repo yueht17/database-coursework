01ff9a65f8c317ef29ac5013855cd0d60cf8ccdb
yueht17 <yueht17@mails.tsinghua.edu.cn>
2020-05-27 21:17:53 +0800

    修改index视图函数，使之可以提交表单到数据库

--- app/main/views.py
+++ app/main/views.py
@@ -1,15 +1,35 @@
 from flask import render_template, redirect, url_for, abort, flash
 from flask_login import login_required, current_user
 from . import main
-from .forms import EditProfileForm, EditProfileAdminForm
+from .forms import EditProfileForm, EditProfileAdminForm, ActivityForm
 from .. import db
-from ..models import Role, User
+from ..models import Role, User, Permission, Activity, ActivityStatus
 from ..decorators import admin_required
+from datetime import datetime
 
 
-@main.route('/')
+@main.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    form = ActivityForm()
+    if current_user.can(Permission.PUBLISH_ACTIVITY) and form.validate_on_submit():
+        if form.begin.data.__ge__(form.end.data):
+            flash("begin time should be earlier than end time")
+            return redirect(url_for('.index'))
+        elif form.begin.data.__lt__(datetime.now()):
+            flash("begin time should be later than now time")
+            return redirect(url_for('.index'))
+        activity = Activity(publisher=current_user._get_current_object(),
+                            begin_timestamp=form.begin.data,
+                            end_timestamp=form.end.data,
+                            location=form.location.data,
+                            name=form.name.data,
+                            description=form.description.data,
+                            capacity=form.capacity.data)
+        db.session.add(activity)
+        flash("Publish success")
+        return redirect(url_for('.index'))
+    activities = Activity.query.order_by(Activity.begin_timestamp.desc()).all()
+    return render_template('index.html', form=form, activities=activities)
 
 
 @main.route('/user/<username>')
