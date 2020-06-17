396a2ac7656d0193bfe52234548671bb51961351
yueht17 <yueht17@mails.tsinghua.edu.cn>
2020-06-17 12:17:17 +0800

    添加了分析耗时过高函数的功能

--- manage.py
+++ manage.py
@@ -57,5 +57,13 @@ def generate_fake():
     print("[Info]:Generate fake info Done!")
 
 
+@manager.command
+def profile(length=25, profile_dir=None):
+    from werkzeug.contrib.profiler import ProfilerMiddleware
+    app.wsgi_app = ProfilerMiddleware(app.wsgi_app, restrictions=[length],
+                                      profile_dir=profile_dir)
+    app.run()
+
+
 if __name__ == '__main__':
     manager.run()
